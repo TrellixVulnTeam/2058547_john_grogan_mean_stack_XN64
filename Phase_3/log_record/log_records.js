d3c6b91d6a87ac505887cb67877a72877ee6cccc
johngrogan98 <60016314+johngrogan98@users.noreply.github.com>
2021-08-23 14:57:34 -0500

    log_record done

---
+++ Phase_3/log_record/log_records.js
@@ -0,0 +1,25 @@
+function addUser(){
+    let fs = require("fs")
+    let logs = JSON.parse(fs.readFileSync("logs.json").toString());
+    debugger;
+
+    let readline = require("readline-sync");
+    let fname = readline.question("Enter your First Name: ")
+    let lname = readline.question("Enter your Last Name: ")
+    let age = readline.questionInt("Enter your Age: ")
+    let gender = readline.question("Enter your gender: ")
+    debugger;
+
+    var currentdate = new Date(); 
+    let date = currentdate.getDate() + "/" + (currentdate.getMonth()+1) + "/" + currentdate.getFullYear()
+    let time = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+    debugger;
+
+    let entry = {fname:fname, lname:lname, age:age, gender:gender, date:date, time:time}
+    logs.push(entry)
+    debugger;
+
+    fs.writeFileSync("logs.json", JSON.stringify(logs))
+}
+
+addUser()
